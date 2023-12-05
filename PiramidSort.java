92b370bd2411d8ff75ff7d1311f71fc2ecf56312
Konstantin7474 <124039227+Konstantin7474@users.noreply.github.com>
2023-12-05 17:06:00 +0800

    Initial_commit

---
+++ PiramidSort.java
@@ -0,0 +1,51 @@
+public class PiramidSort {
+
+    public static void main(String[] args) {
+        int[] array = new int[] {
+                1, 2, 3, 4, 5, 6, 7, 8, 9
+        };
+
+        array = sort(array);
+
+
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static int[] sort(int[] array) {
+
+        for (int i = array.length / 2 - 1; i >= 0; i--)
+            heapify(array, array.length, i);
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+
+            heapify(array, i, 0);
+        }
+
+        return array;
+    }
+
+    private static void heapify(int[] array, int heapSize, int rootIndex) {
+        int largest = rootIndex;
+        int leftChild = 2 * rootIndex + 1;
+        int rightChild = 2 * rootIndex + 2;
+
+        if (leftChild < heapSize && array[leftChild] > array[largest])
+            largest = leftChild;
+
+        if (rightChild < heapSize && array[rightChild] > array[largest])
+            largest = rightChild;
+
+        if (largest != rootIndex) {
+            int temp = array[rootIndex];
+            array[rootIndex] = array[largest];
+            array[largest] = temp;
+
+            heapify(array, heapSize, largest);
+        }
+    }
+}
